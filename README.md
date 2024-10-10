c2eaa37a895d0ec55af2fb59a9d7290a40072ad6
Stefan Jahns <jahns@informatik.uni-leipzig.de>
2024-10-10 10:22:21 +0200

    Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Requirements
 Python Version 3.11
+
+## Overview
+It's just a mesh together from Extended Logic (https://github.com/davidfa71/Extending-Logic) and dimagic(https://doi.org/10.5281/zenodo.12707100) with Docker to quickly generate BDDs from dimacs. 
+Both Extended Logic and dimagic are not from me.
---
+++ dimacs2bdd.py
@@ -0,0 +1,19 @@
+import subprocess
+import os
+import sys
+from Logic2BDD.pylogic2bdd import main as pylogic2bdd_main
+
+def convert_dimacs_to_bdd(input_dimacs, output_folder):
+    # Erster Schritt: dimagic ausführen
+    subprocess.run(['python3.11', 'dimagic/dimagic.py', input_dimacs, '-rv', '-rc', '-l2b', '-o', output_folder])
+
+    # Extrahiere Name und Ordner für pylogic2bdd
+    name = os.path.splitext(os.path.basename(input_dimacs))[0]
+    folder = os.path.basename(os.path.dirname(input_dimacs))
+
+    # Setze die Argumente für pylogic2bdd und führe die Hauptfunktion aus
+    sys.argv = ['pylogic2bdd.py', f"{output_folder}/{folder}", name]
+    pylogic2bdd_main()
+
+# Beispielaufruf der Funktion
+# convert_dimacs_to_bdd("/home/jahns/BDD_Docker/test_data/Mendonca2009.dimacs", "output")
