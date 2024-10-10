955e065a5bf2b6a372c7dd24a207a32c9a7dac19
Stefan Jahns <jahns@informatik.uni-leipzig.de>
2024-10-10 10:28:27 +0200

    Update Readme

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dimacs2BDD",  # Der Name des Projekts
+    version="0.1",  # Versionsnummer
+    packages=find_packages(),  # Findet alle Pakete automatisch
+    install_requires=[],  # Abhängigkeiten, falls vorhanden
+    description="A dimacs to BDD Docker project",  # Kurze Beschreibung
+    author="Stefan",  # Dein Name oder Team
+    url="https://github.com/Stefan577/BDD_Docker",  # URL zu deinem Projekt
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',  # Mindestanforderung für Python-Version
+)
